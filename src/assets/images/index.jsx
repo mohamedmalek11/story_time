780472d0bfa585645d346ed6bf6ee4b346104524
mohamedmalek11 <mohamed.malek.msm@gmail.com>
2025-07-25 20:42:25 +0300

    Added Icons

--- src/assets/images/index.jsx
+++ src/assets/images/index.jsx
@@ -1,5 +1,6 @@
-import user from "./user.svg";
-import search from "./search.svg";
-
-export { ReactComponent as UserIcon } from './user.svg';
-export { ReactComponent as SearchIcon } from './search.svg';
+export { ReactComponent as UserIcon } from "./user.svg";
+export { ReactComponent as SearchIcon } from "./search.svg";
+export { ReactComponent as ProphetsIcon } from "./Prophets.svg";
+export { ReactComponent as PrincessIcon } from "./Princess.svg";
+export { ReactComponent as AnimalsIcon } from "./Animal.svg";
+export { ReactComponent as FairyTalesIcon } from "./Fairy-Tales.svg";
